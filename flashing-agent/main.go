6ac80e608f5ded37b7fb597815dfc3377500e29b
Philippe Voinov <philippevoinov@gmail.com>
2018-04-15 22:14:30 +0200

    Print system info on connection.

--- flashing-agent/main.go
+++ flashing-agent/main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"os"
 
+	"github.com/jaypipes/ghw"
+
 	"git.dolansoft.org/philippe/softmetal/flashing-agent/disk"
 	"git.dolansoft.org/philippe/softmetal/flashing-agent/partition"
 	"git.dolansoft.org/philippe/softmetal/flashing-agent/superlog"
@@ -18,11 +20,8 @@ import (
 var managerHP = flag.String("manager", "", "host and GRPC port of flashing manager (required)")
 
 func flash(logger *superlog.Logger, config *pb.FlashingConfig) error {
-	targetDiskSerial := "TOSHIBA_THNSFJ256GCSU_46KS117IT8LW"
-	// TODO load and check inputs
-
-	logger.Logf("Using disk with serial %v.", targetDiskSerial)
-	f, diskInfo, e := disk.OpenBySerial(targetDiskSerial)
+	logger.Logf("Using disk with serial %v.", config.TargetDiskCombinedSerial)
+	f, diskInfo, e := disk.OpenBySerial(config.TargetDiskCombinedSerial)
 	if e != nil {
 		return e
 	}
@@ -43,6 +42,28 @@ func flash(logger *superlog.Logger, config *pb.FlashingConfig) error {
 	return nil
 }
 
+func logSysinfo(logger *superlog.Logger) error {
+	bl, e := ghw.Block()
+	if e != nil {
+		return e
+	}
+	logger.Logf("block info: %v", bl.String())
+	logger.Logf("  disks: ")
+	for _, v := range bl.Disks {
+		logger.Logf("    %v", v.String())
+	}
+	n, e := ghw.Network()
+	if e != nil {
+		return e
+	}
+	logger.Logf("network info: %v", n.String())
+	logger.Logf("  NICs: ")
+	for _, v := range n.NICs {
+		logger.Logf("    %v", v.String())
+	}
+	return nil
+}
+
 func listen(logger *superlog.Logger) (pb.PowerControlType, error) {
 	var defaultPowerControl pb.PowerControlType
 
@@ -60,8 +81,12 @@ func listen(logger *superlog.Logger) (pb.PowerControlType, error) {
 	logger.AttachSupervisor(c, cmd.SessionId)
 	defer logger.DetachSupervisor()
 
+	if e = logSysinfo(logger); e != nil {
+		logger.Logf("failed to get system info: %v", e)
+	}
+
 	if e = flash(logger, cmd.Config); e != nil {
-		// log this here so that supervisor gets it, since it will be detatched later
+		// Log this here so that supervisor gets it, since it will be detatched later.
 		logger.Logf("flashing error: %v", e)
 	}
 	return cmd.PowerOnCompletion, e
